c763f422851b9a08d58e08b07aeca43f71bf8d88
Lee-sojung <thwjd3698@gmail.com>
2021-11-23 11:35:06 +0900

    step4 isCheck인증함수

--- js/validation.js
+++ js/validation.js
@@ -4,7 +4,9 @@ var btnSubmit = form.querySelector("input[type=submit]");
 btnSubmit.addEventListener("click", function(e){
    if(!isTxt("userid", 5)) e.preventDefault();
    if(!isTxt("comments", 20)) e.preventDefault();
-   if(!isEmail("email", 5)) e.preventDefault();
+   if(!isEmail("email")) e.preventDefault();
+   if(!isCheck("gender")) e.preventDefault();
+   if(!isCheck("hobby")) e.preventDefault();
 });
 
 //text 인증함수
@@ -34,7 +36,7 @@ function isEmail(name, len){
    var input = form.querySelector(`[name=${name}]`);
    var txt = input.value;
 
-   if(txt.length >len && /@/.test(txt)){ //원래 txt.length >0
+   if(/@/.test(txt)){ //원래 txt.length >0 // txt.includes("@")로 특정 문자열 찾기도 가능
       var errMsgs = input.closest("td").querySelectorAll("p");
       if(errMsgs.length >0) input.closest("td").querySelector("p").remove();
       return true;
@@ -43,8 +45,33 @@ function isEmail(name, len){
       if(errMsgs.length >0) input.closest("td").querySelector("p").remove();
 
       var errMsg = document.createElement("p");
-      errMsg.append(`이메일 주소를 ${len}글자 이상 입력하세요`);
+      errMsg.append("@를 포함한 전체 이메일 주소를 입력하세요");
       input.closest("td").append(errMsg);
       return false;
    }
+}
+
+//check 인증함수 정의 gender,hobby
+function isCheck(name){
+   var inputs = form.querySelectorAll(`[name=${name}]`);
+   var isChecked = false;
+
+   for(var input of inputs){
+      if(input.checked) isChecked = true;
+   }
+
+   if(isChecked){
+      var errMsgs = inputs[0].closest("td").querySelectorAll("p");
+      if(errMsgs.length >0) inputs[0].closest("td").querySelector("p").remove();
+      return true;
+   }else{
+      var errMsgs = inputs[0].closest("td").querySelectorAll("p");
+      if(errMsgs.length >0) inputs[0].closest("td").querySelector("p").remove();
+
+      var errMsg = document.createElement("p");
+      errMsg.append("필수 입력사항을 체크하세요");
+      inputs[0].closest("td").append(errMsg);
+      return false;
+   }
+
 }
